596f7d8fb1987bf3207e9c060217709808ec108d
Nemanja Panić <27952111+npanich@users.noreply.github.com>
2023-08-05 22:19:47 +0200

    fixed typo 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ if streamlit.button('Get fruit load list'):
     streamlit.dataframe(my_data_rows)
 
 def insert_row_snowflake(new_fruit):
-    with my_cns.cursor() as my_cur:
+    with my_cnx.cursor() as my_cur:
         my_cur.execute("insert into fruit_load_list ('" + new_fruit + "')")
         return 'Thanks for adding ' + new_fruit
 
